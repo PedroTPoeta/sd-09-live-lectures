f23f19d9090e2ea6330993f84077ca919826d3ae
Gabriel Oliva <gfpoliva@gmail.com>
2020-11-25 17:13:06 -0300

    Aula 4.1

---
+++ script.js
@@ -0,0 +1,8 @@
+let age = 20;
+let person = 'Luciano Lodi';
+
+if (age <= 17) {
+  console.log(person + ', voce nao pode dirigir ☹️');
+} else {
+  console.log(person + ', voce pode dirigir! 🎉');
+}
