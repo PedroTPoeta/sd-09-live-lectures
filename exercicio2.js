63c5728cdbb6513258e202096fc215d923f0e70c
Gabriel Oliva <gfpoliva@gmail.com>
2020-12-01 14:02:23 -0300

    Exercicio 5 da parte 2

---
+++ exercicio2.js
@@ -0,0 +1,28 @@
+// Crie uma função que receba um array de inteiros e retorne o inteiro que mais se repete.
+// Array de teste: [2, 3, 2, 5, 8, 2, 3]; .
+// Valor esperado no retorno da função: 2 .
+
+function numberMostRepeatedFrom(numeros) {
+  let contRepetido = 0;
+  let contNumero = 0;
+  let indexNumeroRepetido = 0;
+
+  for (let index in numeros) {
+    let verificaNumero = numeros[index];
+    for (let index2 in numeros) {
+      if (verificaNumero === numeros[index2]) {
+        contNumero++;
+      }
+    }
+    if (contNumero > contRepetido) {
+      contRepetido = contNumero;
+      indexNumeroRepetido = index;
+    }
+    contNumero = 0;
+  }
+  
+  return numeros[indexNumeroRepetido];
+}
+
+const testArray = [8, 8, 3, 5, 8, 8, 8];
+console.log(numberMostRepeatedFrom(testArray));
